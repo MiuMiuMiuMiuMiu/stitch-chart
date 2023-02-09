a4d17d3ecb78066a6bc6bedd94bfc0fecea6dd50
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2023-02-09 15:14:39 +0100

    Add quantity limit to gauge input

--- src/components/GaugeInput.vue
+++ src/components/GaugeInput.vue
@@ -1,8 +1,8 @@
 <template>
     <label for="height">Height: </label>
-    <input v-model="height" type="number" id="height">
+    <input v-model="height" type="number" id="height" min="1" max="50">
     <label for="width">Width: </label>
-    <input type="number" id="width"> 
+    <input type="number" id="width" min="1" max="50"> 
 </template>
 
 <script>
