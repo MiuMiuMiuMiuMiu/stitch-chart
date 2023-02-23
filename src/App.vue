38803dcd5b5931dd22d83f5964172122b117cf29
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2023-02-23 10:43:42 +0100

    Revert "Add typescript to vue"
    This reverts commit d79fa5a8e84f346dc41de429e292dbbeb41e40ad.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <WelcomeUser name="Anna" />
+    <CreateChart />
+  </div>
+  
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+<script>
+import WelcomeUser from './components/WelcomeUser.vue'
+import CreateChart from './components/CreateChart.vue';
 
-@Options({
+export default {
+  name: 'App',
   components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+    WelcomeUser,
+    CreateChart
+  }
+}
 </script>
 
 <style>
@@ -20,7 +24,6 @@ export default class App extends Vue {}
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
