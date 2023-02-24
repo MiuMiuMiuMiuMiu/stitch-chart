f53af640405c3972bfa466fe5be5771804617786
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2023-02-24 19:47:08 +0100

    Generate grid chart based on input text. 0 = black 1 = white

--- src/components/CreateChart.vue
+++ src/components/CreateChart.vue
@@ -1,27 +1,45 @@
 <template>
   <div>
     <h1>Class</h1>
-    {{ text }}
-    <div id="grid">   
+    <div id="grid" :style="gridStyle">
+      <template v-for="(item, index) in text" :key="index">
+        <div class="grid-cell" v-bind:style= "[item === '0' ? {'background-color': 'grey'} : {'background-color': 'white'}]">
+        </div>
+      </template>
     </div>
   </div>
-  
 </template>
 
 <script>
-
 export default {
-    name: 'CreateChart',
-    props: {
-      text: String
+  name: 'CreateChart',
+  props: {
+    text: String
+  },
+  data() {
+    return {
+      columns: parseInt(this.text.length) 
     }
+  },
+  computed: {
+    gridStyle() {
+      return {
+        gridTemplateColumns: `repeat(${this.text.length}, 50px)`, //Amount of columns from length of text
+        //border: 'black solid 1px'
+      }
+    },
+  }
 }
 </script>
 
 <style scoped>
-
 #grid {
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 1px;
+}
+
+.grid-cell {
+  box-shadow: 0px 0px 0px 1px black;
+  height: 50px;
 }
 </style>
