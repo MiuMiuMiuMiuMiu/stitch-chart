ef1844dcfba934acc80c9b5eea69120dfff08b74
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2023-04-10 17:25:24 +0200

    Get image pixel list

--- src/views/AsciiPage.vue
+++ src/views/AsciiPage.vue
@@ -4,6 +4,7 @@
         <input type="file" id="file-input" ref="image">
         <button type="button" @click="generateAsciiArt">Generate ASCII Art</button>
     </form>
+    <canvas id="canvas"></canvas>
 </template>
 
 <script>
@@ -11,8 +12,13 @@ export default {
     name: "AsciiPage",
     methods: {
         generateAsciiArt() {
+            //Get canvas
+            const canvas = document.getElementById("canvas");
+            const ctx = canvas.getContext("2d");
+
             // Read the contents of those image file    
             const input_image = this.$refs.image.files[0];
+
             //Asynchronously read the contents of image file
             const reader = new FileReader();
             reader.readAsDataURL(input_image);
@@ -23,14 +29,18 @@ export default {
                 image.src = imageDataUrl;
 
                 image.onload = () => {
-                    
-                }
-                // Use image processing library to convert image to ASCII art
-                //const asciiArt = convertToAsciiArt(imageDataUrl);
-                // Display the ASCII art on the webpage
-                //asciiArtDiv.innerText = asciiArt;
-            };
 
+                    canvas.width = image.width;
+                    canvas.height = image.height;
+
+                    //Put image on canvas
+                    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+                    //Get pixel data
+                    const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+                    console.log(pixels)
+                }
+            };
         }
     }
 }
